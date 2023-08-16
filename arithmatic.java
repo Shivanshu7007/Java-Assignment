ffddb5406f70972f014991d82f9e79d011a99fcd
Shivanshu7007 <shivanshumishra6688@gmail.com>
2023-08-17 00:03:31 +0530

    a2

---
+++ arithmatic.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class arithmatic{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter First Number");
+        int a=sc.nextInt();
+        System.out.println("Enter Second Number");
+        int b=sc.nextInt();
+        System.out.println("The Sum Is "+(a+b));
+        System.out.println("The dif Is "+(a-b));
+        System.out.println("The product Is "+(a*b));
+        System.out.println("The div Is "+(a/b));
+        System.out.println("The rem Is "+(a%b));
+    }
+}
