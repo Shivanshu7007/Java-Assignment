214d460e7d1f425f00e6ef8206244a525f950c91
Shivanshu7007 <shivanshumishra6688@gmail.com>
2023-08-17 14:21:17 +0530

    a5

---
+++ even_odd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class even_odd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int num=sc.nextInt();
+        if (num%2==0)
+        System.out.println("Even Number");
+        else
+        System.out.println("Odd number");
+    }    
+}
