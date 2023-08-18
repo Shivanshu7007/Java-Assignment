6ed59e03a7648524019aa77620cd63038de3be6e
Shivanshu7007 <shivanshumishra6688@gmail.com>
2023-08-18 14:49:00 +0530

    a11

---
+++ prime_range.java
@@ -0,0 +1,23 @@
+//Prime number within a given range
+import java.util.Scanner;
+public class prime_range {
+    public static void main(String[] args) {
+        int a,b,j;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the Min limit");
+        a=sc.nextInt();
+        System.out.println("Enter the Max limit");
+        b=sc.nextInt();
+        for(int i=a; i<=b; i++)
+        {
+            for(j=2; j<i; j++)
+            {
+                if(i%j==0)
+                break;
+            }
+            if(i==j)
+            System.out.print(j+" ");   
+        }
+    }
+    
+}
