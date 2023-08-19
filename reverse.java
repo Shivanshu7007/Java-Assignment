026e51464412308807aae12c00ece3ea097d959e
Shivanshu7007 <shivanshumishra6688@gmail.com>
2023-08-19 21:56:45 +0530

    a15

---
+++ reverse.java
@@ -0,0 +1,17 @@
+//Reverse a given number in Java
+import java.util.Scanner;
+public class reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n,r;
+        System.out.println("Enter the Number");
+        n=sc.nextInt();
+        while(n!=0)
+        {
+         r=n%10;
+         n/= 10;
+         System.out.print(r);
+        }
+    }
+    
+}
