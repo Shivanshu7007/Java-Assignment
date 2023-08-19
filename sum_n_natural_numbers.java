1f16c2abddce6001505a16a8ecead9ed46281707
Shivanshu7007 <shivanshumishra6688@gmail.com>
2023-08-19 21:11:56 +0530

    a14

--- sum_n_natural_numbers.java
+++ sum_n_natural_numbers.java
@@ -4,11 +4,11 @@ public class sum_n_natural_numbers {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter The Value of N");
         int n=sc.nextInt();
-        /* n=n*(n+1)/2;
-           System.out.println(n); */
-        int sum =0;
-        for (int i=1 ; i<=n ; ++i)
+        n=n*(n+1)/2; // formula for finding sum of n natural numbers
+        System.out.println(n); 
+        /*int sum = 0;
+        for (int i=1 ; i<=n ; i++)     By using for loop
         sum += i;
-        System.out.println(sum);
+        System.out.println(sum);*/
     }    
 }
